1ad3360d797a81931b454fe3a437d8b5e36c328b
haenlee <85219306+haenlee@users.noreply.github.com>
2022-10-08 15:45:36 +0900

    Revert "[#20] Rider API 추가"

--- src/main/java/com/deliveryfood/controller/RiderController.java
+++ src/main/java/com/deliveryfood/controller/RiderController.java
@@ -1,55 +1,32 @@
 package com.deliveryfood.controller;
 
-import com.deliveryfood.model.LoginResult;
-import com.deliveryfood.model.RiderInput;
+import com.deliveryfood.model.UserInput;
 import com.deliveryfood.model.UserRequest;
-import com.deliveryfood.service.IRiderService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-
-@RequiredArgsConstructor
-@RestController
+@Controller
 @RequestMapping("/riders")
 public class RiderController {
 
-    private final IRiderService riderService;
-
     @PostMapping("/certification")
-    public void certification(@RequestParam String code) {
+    public void certification(@RequestParam int code) {
         // 입력한 코드로 본인 인증
-        riderService.certification(code);
     }
 
     @PostMapping("/register")
-    public void register(@RequestBody RiderInput riderInput) {
+    public void register(UserInput userInput) {
         // 라이더 회원 가입
-        riderService.register(riderInput);
     }
 
     @PostMapping("/withdraw")
-    public void withdraw(@RequestBody UserRequest userRequest) {
+    public void withdraw(UserRequest userRequest) {
         // 라이더 회원 탈퇴 (session을 삭제할 뿐 정보의 변경은 없다.)
-        riderService.withdraw(userRequest);
     }
 
     @PostMapping("/login")
-    public HttpStatus login(HttpServletRequest request) {
-        // 로그인
-        UserRequest userRequest = new UserRequest(request.getParameter("username"), request.getParameter("password"));
-        LoginResult result = riderService.login(userRequest);
-        if(result.equals(LoginResult.NOT_REGISTER_AUTH)) {
-            return HttpStatus.FOUND;
-        }
-
-        return HttpStatus.OK;
+    public void login(UserRequest userRequest) {
+        // 라이더 회원 로그인
     }
 
     @PostMapping("/logout")
@@ -57,9 +34,13 @@ public class RiderController {
         // 라이더 회원 로그아웃
     }
 
+    @GetMapping("/{userId}")
+    public void findUser(@PathVariable int userId) {
+        // 라이더 회원 조회
+    }
+
     @PutMapping("/{userId}")
-    public void modifyUser(@RequestBody RiderInput riderInput) {
-        // 라이더 회원 정보 수정 (현재는 커미션만 수정 가능)
-        riderService.modifyRider(riderInput);
+    public void modifyUser(@PathVariable int userId) {
+        // 라이더 회원 정보 수정
     }
 }
--- src/test/java/com/deliveryfood/controller/RiderControllerTest.java
+++ src/test/java/com/deliveryfood/controller/RiderControllerTest.java
@@ -1,8 +1,6 @@
 package com.deliveryfood.controller;
 
-import com.deliveryfood.model.RiderInput;
-import com.deliveryfood.model.UserRequest;
-import com.deliveryfood.service.MemberService;
+import com.deliveryfood.model.UserInput;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,14 +9,12 @@ import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -30,30 +26,16 @@ public class RiderControllerTest {
     @Autowired
     private RiderController riderController;
 
-    @Autowired
-    private FilterChainProxy filterChainProxy;
-
     @BeforeEach
     public void init() {
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(riderController)
-                .apply(springSecurity(filterChainProxy))
-                .build();
+        mockMvc = MockMvcBuilders.standaloneSetup(riderController).build();
     }
 
     @Test
     @DisplayName("라이더 회원가입시 본인 인증을 처리한다.")
     public void testCertification() throws Exception {
-        UserRequest userRequest = UserRequest.builder()
-                .email("rider@gmail.com")
-                .build();
-
-        ObjectMapper objectMapper = new ObjectMapper();
         mockMvc.perform(post("/riders/certification")
-                        .characterEncoding("utf-8")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userRequest))
-                        .param("code", MemberService.REGISTER_CODE))
+                .param("code", String.valueOf(ArgumentMatchers.anyInt())))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -61,19 +43,19 @@ public class RiderControllerTest {
     @Test
     @DisplayName("라이더 회원 가입을 한다")
     public void testRegister() throws Exception {
-        RiderInput riderInput = RiderInput.builder()
-                .name("라이더")
-                .email("rider@gmail.com")
-                .password("riderpassword")
+        UserInput userInput = UserInput.builder()
+                .name("테스트")
+                .email("test@gmail.com")
+                .password("testpassword")
                 .phone("010-1234-5678")
-                .commission(3000)
+                .address("서울시 구로구 디지털로")
                 .build();
 
         ObjectMapper objectMapper = new ObjectMapper();
         mockMvc.perform(post("/riders/register")
                 .characterEncoding("utf-8")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(riderInput)))
+                .content(objectMapper.writeValueAsString(userInput)))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -81,16 +63,15 @@ public class RiderControllerTest {
     @Test
     @DisplayName("라이더 회원 탈퇴를 한다.")
     public void testWithdraw() throws Exception {
-        UserRequest userRequest = UserRequest.builder()
-                .email("rider@gmail.com")
-                .password("riderpassword")
-                .build();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.set("userId", String.valueOf(ArgumentMatchers.anyInt()));
+        map.set("password", "testpassword");
 
         ObjectMapper objectMapper = new ObjectMapper();
         mockMvc.perform(post("/riders/withdraw")
                 .characterEncoding("utf-8")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userRequest)))
+                .content(objectMapper.writeValueAsString(map)))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -98,14 +79,15 @@ public class RiderControllerTest {
     @Test
     @DisplayName("라이더 회원 로그인을 한다.")
     public void testLogin() throws Exception {
-        UserRequest userRequest = UserRequest.builder()
-                .email("rider@gmail.com")
-                .password("riderpassword")
-                .build();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.set("userId", String.valueOf(ArgumentMatchers.anyInt()));
+        map.set("password", "testpassword");
 
-        mockMvc.perform(formLogin()
-                        .user(userRequest.getEmail())
-                        .password(userRequest.getPassword()))
+        ObjectMapper objectMapper = new ObjectMapper();
+        mockMvc.perform(post("/riders/login")
+                .characterEncoding("utf-8")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(map)))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
@@ -114,6 +96,15 @@ public class RiderControllerTest {
     @DisplayName("라이더 회원 로그아웃을 한다.")
     public void testLogout() throws Exception {
         mockMvc.perform(post("/riders/logout"))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("userId로부터 라이더 회원을 조회한다.")
+    public void testFindUser() throws Exception {
+        int userId = ArgumentMatchers.anyInt();
+        mockMvc.perform(get("/riders/" + userId))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
