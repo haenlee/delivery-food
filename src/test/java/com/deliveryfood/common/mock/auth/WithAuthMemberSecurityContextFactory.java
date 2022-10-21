6d7e0a00331b62180c7ae7a07f46a804f3612190
haenlee <85219306+haenlee@users.noreply.github.com>
2022-10-21 23:14:56 +0900

    feature: 테스트시 중복 Authority를 가질 수 있도록 수정

--- src/test/java/com/deliveryfood/common/mock/auth/WithAuthMemberSecurityContextFactory.java
+++ src/test/java/com/deliveryfood/common/mock/auth/WithAuthMemberSecurityContextFactory.java
@@ -4,12 +4,14 @@ import com.deliveryfood.model.CustomUserDetails;
 import com.deliveryfood.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RequiredArgsConstructor
 public class WithAuthMemberSecurityContextFactory implements WithSecurityContextFactory<WithAuthMember> {
@@ -23,10 +25,18 @@ public class WithAuthMemberSecurityContextFactory implements WithSecurityContext
         CustomUserDetails user = memberService.loadUserByUsername(username);
 
         UsernamePasswordAuthenticationToken token =
-                new UsernamePasswordAuthenticationToken(user, user.getPassword(), List.of(new SimpleGrantedAuthority(authority)));
+                new UsernamePasswordAuthenticationToken(user, user.getPassword(), getAuthorities(authority));
 
         SecurityContext context = SecurityContextHolder.getContext();
         context.setAuthentication(token);
         return context;
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(String authority) {
+        ArrayList<GrantedAuthority> authorityList = new ArrayList<>();
+        for(String role : authority.split(",")) {
+            authorityList.add(new SimpleGrantedAuthority(role));
+        }
+        return authorityList;
     }
 }
