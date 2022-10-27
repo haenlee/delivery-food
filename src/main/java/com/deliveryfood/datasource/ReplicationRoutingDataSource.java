edbf5da94ece8019b5228b19ac09cf3cc8975ef2
cyh <drsggg>
2022-10-27 13:51:21 +0900

    fix : 트랜잭션 매니저, DB분리 수정#2

--- src/main/java/com/deliveryfood/datasource/ReplicationRoutingDataSource.java
+++ src/main/java/com/deliveryfood/datasource/ReplicationRoutingDataSource.java
@@ -4,8 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-import java.util.Map;
-
 @Slf4j
 public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
 
