09469aed7adab6b7f07b29d782d412b531ca3aa0
haenlee <85219306+haenlee@users.noreply.github.com>
2022-11-07 02:16:16 +0900

    refactor: lombok* wildcard 제거

--- src/main/java/com/deliveryfood/dto/RiderDto.java
+++ src/main/java/com/deliveryfood/dto/RiderDto.java
@@ -1,6 +1,10 @@
 package com.deliveryfood.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.time.LocalDateTime;
 
